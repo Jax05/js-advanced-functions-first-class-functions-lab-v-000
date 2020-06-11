98ac90e818e5a21103bd5454ff52e5758724c109
Carley Tripp <carley.tripp@outlook.com>
2020-06-11 01:42:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(-2)
 }
 
-const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
 function createFareMultiplier(multiplier) {
   return function(fare) {
