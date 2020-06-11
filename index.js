cf797242ad7226931dfd1844125760c6071a7fb3
Carley Tripp <carley.tripp@outlook.com>
2020-06-11 01:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  drivers.slice(0, 3)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  drivers.slice(-2)
+}
