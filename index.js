dcc40f5326dab9e872bc392f5aaecf9b40507d9b
Carley Tripp <carley.tripp@outlook.com>
2020-06-11 01:52:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function createFareMultiplier(multiplier) {
 }
 
 const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, position) {
+  return position
+}
